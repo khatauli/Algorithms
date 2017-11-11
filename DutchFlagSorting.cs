5a94b8e5d839472480e11497e70e185a4273d858
khatauli <sachin.kumar.v@gmail.com>
2017-11-11 05:20:22 -0800

    added two algo codes

---
+++ DutchFlagSorting.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2 };
+
+            DutchFlagAlgo(array);
+
+        }
+
+        //this works for negative as well the positive numbersA
+        static void DutchFlagAlgo(int[] array)
+        {
+            int p1 = 0;
+            int p2 = array.Length - 1;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i >= p2) break;
+
+                int val = array[i];
+
+                if (val == 0)
+                {
+                    if (i > p1)
+                    {
+                        array[i] = array[p1];
+                        array[p1] = val;
+                    }
+                    p1++;
+                }
+                else if (val == 2)
+                {
+                    array[i] = array[p2];
+                    array[p2] = val;
+                    p2--;
+                    i--;
+                }
+            }
+        }
+    }
+}
---
+++ MaxSumSubArray.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { -3, -5, -8, 6, -1, -3, -8 };
+
+            var value = ReturnSubarray(array);
+
+        }
+        //this works for negative as well the positive numbersA
+        static int[] ReturnSubarray(int[] array)
+        {
+            int maxSum = int.MinValue;
+            int maxStart = 0;
+            int maxEnd = 0;
+
+            int currentSum = 0;
+            int currentStart = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                currentSum += array[i];
+
+                if (currentSum > maxSum)
+                {
+                    maxSum = currentSum;
+                    maxStart = currentStart;
+                    maxEnd = i;
+                }
+
+                if (currentSum < 0)
+                {
+                    currentStart = i + 1;
+                    currentSum = 0;
+                }
+            }
+
+            return null;
+        }
+
+    }
+}
